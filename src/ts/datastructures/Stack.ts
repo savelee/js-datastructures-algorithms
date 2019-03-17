5c6ec0e0f85c5a9d3ae95b998b89838ec41100c9
Lee Boonstra <lee@leeboonstra.com>
2019-03-17 14:21:58 +0100

    Empty stack class

--- src/ts/datastructures/Stack.ts
+++ src/ts/datastructures/Stack.ts
@@ -1,3 +1,17 @@
+/*
+ * Stack DataStructure
+ * @author Lee Boonstra
+ * 
+ *      # What do we know about Stacks?
+ *      It's a sequential data structure with LIFO. Last In - First Out principle.
+ *      Compare this to a deck of cards. The last card on top, is the first
+ *      card to be removed from the pile. The base card, will be last.
+ *
+ *      ## Real world examples in Software Engineering?
+ *      The browser history. Clipboard history.
+ *      In the Sencha Ext JS framework there was a card layout / animation.
+ *      The deck of cards in Hearth Stone.
+ */
 export class Stack {
     private items: any;
     private count: 0;
@@ -7,9 +21,69 @@ export class Stack {
         this.count = 0;
     }
 
+    /*
+     * Add a new item to the top of the stack.
+     * Take O(1) time. We do not run any loop in any of these operations.
+     *
+     * @param item String
+     */
     push() {
-        console.log('push');
-        console.log(this.items);
-        console.log(this.count);
+  
     }
+
+    /*
+     * Pops / Removes the top item from the stack.
+     * Take O(1) time. We do not run any loop in any of these operations.
+     *
+     * @return removed item
+     */
+    pop() {
+
+    }
+
+    /*
+    * Return the top item from the stack.
+    * Take O(1) time. We do not run any loop in any of these operations.
+    *
+    * @return removed item
+    */
+    peek() {
+
+    }
+
+    /*
+    * Check if the Stack is empty. Returns true if the stack has no items
+    * Take O(1) time. We do not run any loop in any of these operations.
+    * @returns Boolean
+    */
+    isEmpty() {
+
+    }
+
+    /*
+    * Removes all the items from the Stack
+    */
+    clear() {
+
+    }
+
+    /*
+    * Return the total number of items from the stack
+    * @returns number
+    */
+    size() {
+
+    }
+
+    /*
+    * Returns the 1-based position where an object is on this stack.
+    * If the object o occurs as an item in this stack, this method returns the distance from the top
+    * of the stack of the occurrence nearest the top of the stack; the topmost item on the stack
+    * is considered to be at distance 1.
+    *
+    * @returns number
+    */
+   search() {
+
+   }
 }
